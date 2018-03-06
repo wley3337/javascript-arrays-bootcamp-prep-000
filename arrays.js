1b05e38a193d774c67c930d77c4a2530903e6d7e
wley3337@gmail.com <wley3337@gmail.com>
2018-03-06 01:18:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,4 @@
-var ingredient1 = "bread";
-var ingredient2 = "mild cheese";
-var ingredient3 = "sharp cheese";
-var ingredient4 = "butter";
-var ingredient5 = "tomato";
-var ingredient6 = "garlic";
+
 var grilledCheeseIngredients = ['bread', 'mild cheese', 'sharp cheese', 'butter', 'tomato', 'garlic'];
 var tomatoSauceIngredients = ['tomato', 'garlic', 'olive oil', 'basil', 'oregano'];
 var chocolatBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
@@ -12,8 +7,8 @@ var myArray = [1,2,3];
 
 
 function addElementToBeginningOfArray(myArray,x){
-  
-  var myNewArray = myArray;myArray.unshift(`x`);
+  var myNewArray = myArray;
+  myNewArray.unshift("x");
   return myNewArray;
 }
 
@@ -21,4 +16,18 @@ function addElementToBeginningOfArray(myArray,x){
 function destructivelyAddElementToEndOfArray(myArray,y){
   myArray[0] = `y`;
   return myArray;
+}
+
+function addElementtoEndOfArray(myArray, z){
+  var myNewEndArray=myArray;
+  myNewEndArray.push("z");
+  return myNewEndArray;
+}
+
+function destructivelyAddElemntToEndOfArray(myArray, i){
+  
+}
+
+function accessElementInArray(myArray, i){
+  
 }
