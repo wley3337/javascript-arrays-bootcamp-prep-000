70de0d3cb8fcf122f5aa1aeea4a09a6f8e78adf6
wley3337@gmail.com <wley3337@gmail.com>
2018-03-06 00:55:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,3 +6,6 @@ var ingredient5 = "tomato";
 var ingredient6 = "garlic";
 var grilledCheeseIngredients = ['bread', 'mild cheese', 'sharp cheese', 'butter', 'tomato', 'garlic'];
 var tomatoSauceIngredients = ['tomato', 'garlic', 'olive oil', 'basil', 'oregano'];
+var chocolatBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+var cities = ["New York", "San Francisco"];
+["Philadelphia", ...cities]
